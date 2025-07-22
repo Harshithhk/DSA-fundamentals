0d7841a3a9e0c61bd5e0d730b76a41e0d31d3547
Harshithhk <dota2kelkar@gmail.com>
2025-07-21 22:22:43 -0400

    added bubble-sort

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,14 @@
+def bubblesort(arr):
+    n = len(arr)
+    for i in range(n - 1, -1, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+
+
+nums = [1, 10, 7, 4, 5]
+sorted_arr = bubblesort(nums)
+print(sorted_arr)
