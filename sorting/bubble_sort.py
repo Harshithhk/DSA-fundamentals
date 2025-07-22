5545d8d283fd092cd64046b99dbbc97e3484fb26
Harshithhk <dota2kelkar@gmail.com>
2025-07-21 22:29:07 -0400

    file cleanup

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,6 +1,6 @@
 def bubblesort(arr):
     n = len(arr)
-    for i in range(n - 1, -1, -1):
+    for i in range(n - 1, 0, -1):
         for j in range(i):
             if arr[j] > arr[j + 1]:
                 temp = arr[j]
